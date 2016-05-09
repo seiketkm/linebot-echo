f976e0511b125eeb8d8a940b75eb0e235d5be751
@seiketkm <takoforest@gmail.com>
2016-05-10 01:43:17 +0900

    modified

--- app.js
+++ app.js
@@ -1,54 +1,47 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const line = require('./line');
 const _ = require('lodash');
+const request = require('request');
 const app = express();
 
 app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/callback', (req, res)=>{
-    console.log('hoge');
-    res.end();
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/callback', (req, res) => {
     //受信
-    console.log("post routed");
-    console.log(req.body);
     _.each(req.body.result, (recvmsg) => {
 	//応答
 	const header = _.defaults(line, {
 	    "Content-Type": 'application/json; charset=UTF-8'
 	});
+	console.log(JSON.stringify(header));
 	const sendmsg = {
-	    'to': [recvmsg.from],
-	    'toChannel': 1383378250, //FixedValue
-            'eventType': '138311608800106203', //FixedValue
-	    'content'  : {
-		"contentType":1, //TextMessageType
-		"toType":1, // User
-		"text": recvmsg.content.text
+	    to: [recvmsg.content.from],
+	    toChannel: 1383378250, //FixedValue
+            eventType: '138311608800106203', //FixedValue
+	    content: {
+		contentType: 1, //TextMessageType
+		toType: 1, // User
+		text: recvmsg.content.text
 	    }
 	};
+	console.log(JSON.stringify(sendmsg));
 	const options = {
 	    url: 'https://trialbot-api.line.me/v1/events',
             headers: header,
             json: true,
             body: sendmsg
-	}
+	};
+	request.post(options, function (err, response, body) {
+            if (err) {
+		console.log(err);
+            }
+            console.log(body);
+	});
     });
-    request.post(options, function (err, response, body) {
-        if (err) {
-            console.log(err);
-        }
-        console.log(body);
-    });
-
     res.end();
 });
 
@@ -66,6 +59,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500).json(err);
+    console.log(err);  
   });
 }
 
@@ -73,6 +67,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500).json({});
+  console.log(err);  
 });
 
 
